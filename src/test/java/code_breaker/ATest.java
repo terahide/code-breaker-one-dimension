bf5ba0ada6937676cbf1485fab034b8e524edeb9
terahide <terahide27@gmail.com>
2016-05-04 10:43:57 +0900

    自動生成したテストを追加

---
+++ src/test/java/code_breaker/ATest.java
@@ -0,0 +1,14 @@
+package code_breaker;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ATest {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
