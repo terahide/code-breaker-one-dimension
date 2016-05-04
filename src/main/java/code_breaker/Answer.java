8eb168e6f6d36be80c352d10dea96eddbd82a5ad
terahide <terahide27@gmail.com>
2016-05-04 11:16:37 +0900

    refactor 重複チェックのコードをメソッドに外出し

--- src/main/java/code_breaker/Answer.java
+++ src/main/java/code_breaker/Answer.java
@@ -6,6 +6,11 @@ import java.util.Set;
 public class Answer {
     final int value; 
     public Answer(int value) {
+        validateDuplicate(value);
+        this.value = value;
+    }
+    
+    private void validateDuplicate(int value) {
         String s = String.valueOf(value);
         Set<Character> set = new HashSet<>();
         for(int i=0;i<s.length();i++){
@@ -14,9 +19,8 @@ public class Answer {
         if ( s.length() != set.size() ){
             throw new IllegalStateException("dupricated number");
         }
-
-        this.value = value;
     }
+    
     public boolean match(int input) {
         if( value == input ){
             return true;
