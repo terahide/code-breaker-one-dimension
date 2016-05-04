34d04b3ebc5a8f4fc342fe671b2c82b3cbbeb9ce
terahide <terahide27@gmail.com>
2016-05-04 11:03:32 +0900

    refactor 重複したコードをまとめた

--- src/test/java/code_breaker/CodeBreakerTest.java
+++ src/test/java/code_breaker/CodeBreakerTest.java
@@ -3,19 +3,25 @@ package code_breaker;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class CodeBreakerTest {
+    
+    Answer sut;
+    
+    @Before
+    public void setup(){
+        sut = new Answer(102);
+    }
 
     @Test
     public void 答えが102の時に102を予想したなら完全一致であるべき() {
-        Answer sut = new Answer(102);
         assertThat(sut.match(102),is(true));
     }
 
     @Test
     public void 答えが102の時に765を予想したなら完全一致でないべき() {
-        Answer sut = new Answer(102);
         assertThat(sut.match(765),is(false));
     }
 }
