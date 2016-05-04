83edfe7e12c42f0bed989279e6724426e786f7e0
terahide <terahide27@gmail.com>
2016-05-04 10:57:27 +0900

    refact テスト名を変更

--- src/test/java/code_breaker/ATest.java
+++ src/test/java/code_breaker/ATest.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 public class ATest {
 
     @Test
-    public void test() {
+    public void 答えが102の時に102を予想したなら完全一致であるべき() {
         Answer sut = new Answer(102);
         assertThat(sut.match(102),is(true));
     }
