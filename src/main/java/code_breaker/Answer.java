a6bd902cedf6c32e9529bab40807e09358c5ddcd
terahide <terahide27@gmail.com>
2016-05-04 10:56:24 +0900

    refact プロダクションコードのclass名を変更

---
+++ src/main/java/code_breaker/Answer.java
@@ -0,0 +1,12 @@
+package code_breaker;
+
+public class Answer {
+    final int value; 
+    public Answer(int value) {
+        this.value = value;
+    }
+    public boolean match(int i) {
+        return true;
+    }
+
+}
--- src/test/java/code_breaker/ATest.java
+++ src/test/java/code_breaker/ATest.java
@@ -3,14 +3,13 @@ package code_breaker;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
-import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 
 public class ATest {
 
     @Test
     public void test() {
-        CodeBreaker sut = new CodeBreaker(102);
+        Answer sut = new Answer(102);
         assertThat(sut.match(102),is(true));
     }
 
