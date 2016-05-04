e8c8b0288b88286d93b330f6f6a0dedf7248fe77
terahide <terahide27@gmail.com>
2016-05-04 11:13:20 +0900

    green 重複した数字はNGになるように修正 明白な実装

--- src/main/java/code_breaker/Answer.java
+++ src/main/java/code_breaker/Answer.java
@@ -1,8 +1,20 @@
 package code_breaker;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Answer {
     final int value; 
     public Answer(int value) {
+        String s = String.valueOf(value);
+        Set<Character> set = new HashSet<>();
+        for(int i=0;i<s.length();i++){
+            set.add(s.charAt(i));
+        }
+        if ( s.length() != set.size() ){
+            throw new IllegalStateException("dupricated number");
+        }
+
         this.value = value;
     }
     public boolean match(int input) {
